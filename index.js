8ec370f2550c6076ac51aa68efb58384e18a1cfe
Stephanie Conde <stephaniecondecareer@gmail.com>
2022-06-30 18:09:18 -0700

    commit tree 0b7715fec9982009b4101ece2d91d52f351d8a95

--- index.js
+++ index.js
@@ -11,6 +11,39 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+
+const titleCased = () =>  {
+ return tutorials.map((string) => string.split (" ").map((letter) => letter[0].toUpperCase() + letter.substring(1)).join(" "))
 }
+  
+
+  //something like this originally passed but I messed it up when I talked to Jennifer and she taught me the above one.
+  // const titleCased = tutorials =>  {
+  //   return tutorials.map((string) => {
+  //     const words = string.split (" ");
+  //     const capWords = words.map((letter) => {
+  //       return letter[0].toUpperCase() + letter.substring(1);
+  //     })
+   
+  //     const capLetter = capWords.join(" ");
+  //     return capLetter
+  //   }
+  //    )}
+  
+ 
+//const titleCased =  
+
+// function titleCased(tutorials) {
+// return tutorials.toLowerCase().split(' ').map(function(word) {
+// return (word.charAt(0).toUpperCase() + word.slice(1));
+// }).join(' ');
+// }
+//titleCased(tutorials);
+
+
+//function titleCase(str) {
+  //return str.toLowerCase().split(' ').map(function(word) {
+    //return (word.charAt(0).toUpperCase() + word.slice(1));
+  //}).join(' ');
+//}
+//titleCase("I'm a little tea pot");
